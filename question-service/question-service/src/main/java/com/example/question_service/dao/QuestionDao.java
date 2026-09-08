57cab769b4557d54e1ecfdf38609d04659c0ceff
JeyaMatheshGanesh <matheshganesh06@gmail.com>
2026-09-08 23:51:47 +0530

    Fix question table name in native query

--- question-service/question-service/src/main/java/com/example/question_service/dao/QuestionDao.java
+++ question-service/question-service/src/main/java/com/example/question_service/dao/QuestionDao.java
@@ -15,7 +15,7 @@ public interface QuestionDao extends JpaRepository<Question,Integer> {
     List<Question> findByCategory(String category);
 
     @Query(
-            value = "SELECT q.id FROM questions q " +
+            value = "SELECT q.id FROM question q " +
                     "WHERE q.category = :category " +
                     "ORDER BY RAND() " +
                     "LIMIT :numQ",
